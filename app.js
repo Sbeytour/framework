8db99b39e049a6893bdd7409fb61793495d2eb57
sbeytour <beytour.safae@gmail.com>
2025-04-29 10:21:35 +0100

    file structure/ start

--- app.js
+++
@@ -1,6 +0,0 @@
-// app.js - نقطة الدخول للتطبيق
-import { createApp } from './framework/framework.js';
-import { TodoApp } from './components/TodoApp.js';
-
-// تركيب التطبيق
-createApp(TodoApp, document.getElementById('root'));
